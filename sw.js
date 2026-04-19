79ab9f29167639752b3bbe28d4334a694cdbe966
adelsontrf-cmd <adelsontrf@gmail.com>
2026-04-19 18:34:31 -0300

    adiciona PDFs e mapas

---
+++ sw.js
@@ -0,0 +1,3 @@
+const C='grc-v2';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./index.html'])))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
